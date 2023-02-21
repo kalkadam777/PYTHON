fef4e3bab89132d3136473b97cd94ae0a8b37673
Erkebulan <kalkadamov14@gmail.com>
2023-02-21 15:07:06 +0600

    something

---
+++ TSIS1/week3/23.py
@@ -0,0 +1,13 @@
+l ='''
+where Ip is the  angular momentum of the frame without weights,
+is the mass of each additional weight, l1is their  distance from 
+the axis of rotation. We will ignore rotational inertia of the stacked bullet,
+because its mass is small com-pared to the mass of the frame and additional weights (3.30 g).
+According to the conservation of angular momentum law: 
+'''
+
+x = l.split( )
+print(x)
+x.reverse()
+for i in x:
+    print(i)
---
+++ TSIS1/week4/Python Math/try to yourself/4.py
@@ -0,0 +1,7 @@
+import math
+
+x = math.sqrt(64)
+
+print(x)
+
+
